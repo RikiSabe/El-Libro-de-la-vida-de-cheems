40842189311749797d261a15bc76c8721b96b7c9
RikiSabe <61205836+RikiSabe@users.noreply.github.com>
2024-03-10 16:54:43 -0400

    Update code.cpp

--- Miselanea/code.cpp
+++ Miselanea/code.cpp
@@ -53,3 +53,15 @@ void reduccion(int n,int d){
 	if ( d == 1 ) cout << n << endl;
 	else cout << n << "/" << d << endl;
 }
+// knight moves...
+int dx[] = {-1,-2,-2,-1,1,2,2,1};
+int dy[] = {-2,-1,1,2,2,1,-1,-2};
+// grid moves without diagonal
+int dx[] = {0,-1,0,1};
+int dy[] = {-1,0,1,0};
+// only all diagonal moves
+int dx[] = {-1,-1,1,1};
+int dy[] = {-1,1,1,-1};
+// grid moves with diagonal
+int dx[] = {0,-1,-1,-1,0,1,1,1};
+int dy[] = {-1,-1,0,1,1,1,0,-1};
