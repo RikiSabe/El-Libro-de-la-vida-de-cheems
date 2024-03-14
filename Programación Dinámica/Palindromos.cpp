4c9962735f319d5cfa4007e2301ded896953e90f
RikiSabe <61205836+RikiSabe@users.noreply.github.com>
2024-03-14 15:52:53 -0400

    Add files via upload

---
+++ Programación Dinámica/Palindromos.cpp
@@ -0,0 +1,15 @@
+int fun(string s){
+    int n = sz(s);
+    vector<vector<int>> dp(n, vector<int> (n,0));
+    FOR(l,2,n + 1){
+        FOR(i,0,n - l + 1){
+            int j = i + l - 1;
+            if( s[i] == s[j] ){
+                dp[i][j] = dp[i + 1][j - 1];
+            }else{
+                dp[i][j] = min(dp[i][j - 1], dp[i + 1][j]) + 1;
+            }
+        }
+    }
+    return dp[0][n-1];
+}
