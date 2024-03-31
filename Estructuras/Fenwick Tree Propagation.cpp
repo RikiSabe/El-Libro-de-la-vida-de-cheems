6e2157a6c88cd07ab412ac09b91b00671e00cfea
LeoCheems <107894780+execute999@users.noreply.github.com>
2024-03-31 11:16:47 -0400

    Update Fenwick Tree Propagation.cpp

--- Estructuras/Fenwick Tree Propagation.cpp
+++ Estructuras/Fenwick Tree Propagation.cpp
@@ -1,25 +1,26 @@
-ll bit1[N],bit2[N];
+ll bit1[N], bit2[N];
 
-void update(ll bit[], int idx, ll val){
+void propagate(ll bit[], int idx, ll val){
     for(int x = idx; x < N; x += x & -x)
         bit[x] += val;
 }
 
-ll query(ll bit[], int idx){
+ll qry(ll bit[], int idx){
     ll ret = 0;
-    for(int x = idx;x > 0;x -= x & -x)
+    for(int x = idx; x > 0;x -= x & -x)
         ret += bit[x];
     return ret;
 }
 
-FOR(i, 0, n) {
-    cin >> v >> l >> r ; // value and indexes to add a value
-    update(bit1,l,v); update(bit1,r + 1,-v);
-    update(bit2,l,-(long long)v * (l - 1)); update(bit2,r + 1,(long long)v * r);
+
+ll qry(ll l, ll r) { // check sum [l, r]
+	return (qry(bit1, r) * r + qry(bit2,r) - qry(bit1,l - 1) * (l - 1) - qry(bit2,l - 1));
 }
 
-// query for [l, r]
-FOR(i , 1, s.size() + 1) {
-	cin >> l >> r;
-    ll val = (query(bit1, r) * r + query(bit2,r) - query(bit1,l - 1) * (l - 1) - query(bit2,l - 1));
+void propagate(ll l, ll r, ll v) { // propagate +v between [l, r]
+	propagate(bit1, l, v); propagate(bit1, r + 1, -v);
+        propagate(bit2, l, -(ll)v * (l - 1)); 
+	propagate(bit2, r + 1, (ll)v * r);
 }
+
+// TODO: Should we use struct????
