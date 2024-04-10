abda7feb297a824224bc58035950b8a208bbf2d3
LeoCheems <107894780+execute999@users.noreply.github.com>
2024-04-10 13:04:02 -0400

    Update Ordered Set.cpp

--- Estructuras/Ordered Set.cpp
+++ Estructuras/Ordered Set.cpp
@@ -1,7 +1,7 @@
+#define typeOfData int
 #include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp>
-
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+#include <ext/pb_ds/tree_policy.hpp> 
+#define ordered_set tree<typeOfData, null_type,less<typeOfData>, rb_tree_tag,tree_order_statistics_node_update> 
 using namespace __gnu_pbds;
 
 // Finding the second smallest element 
