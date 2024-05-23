7336adb3f33ec7558e58d49c49ff59f2e235ba84
LeoCheems <107894780+execute999@users.noreply.github.com>
2024-05-23 10:46:53 -0400

    Create UnionFind.cpp

---
+++ Estructuras/UnionFind.cpp
@@ -0,0 +1,29 @@
+// weighted 
+void make_set(int v) {
+    parent[v] = v;
+    size[v] = 1;
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (size[a] < size[b])
+            swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+    }
+}
+// normal
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b)
+        parent[b] = a;
+}
