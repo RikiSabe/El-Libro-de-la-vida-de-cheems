f1ad356cf68250cf7abc244168c01eec9659a619
LeoCheems <107894780+execute999@users.noreply.github.com>
2024-06-14 15:26:41 -0400

    Create CrossProduct.cpp

---
+++ Geometria/CrossProduct.cpp
@@ -0,0 +1,7 @@
+ ll p = CrossProduct(    
+                            x3 - x1,
+                            y3 - y1,
+                            x3 - x2,
+                            y3 - y2
+                        );
+// check if P3 is in the left(-) or rigth(+)
