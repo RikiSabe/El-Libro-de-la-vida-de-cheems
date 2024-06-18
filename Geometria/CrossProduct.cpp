9ac530d3c149ca42815343271df4a8363e2d25c0
LeoCheems <107894780+execute999@users.noreply.github.com>
2024-06-18 10:00:43 -0400

    Update CrossProduct.cpp

--- Geometria/CrossProduct.cpp
+++ Geometria/CrossProduct.cpp
@@ -1,4 +1,8 @@
- ll p = CrossProduct(    
+ll CrossProduct(ll x1 , ll y1 , ll x2, ll y2){
+	return (x1 * y2) - (y1 * x2);
+}
+
+ll p = CrossProduct(    
                             x3 - x1,
                             y3 - y1,
                             x3 - x2,
