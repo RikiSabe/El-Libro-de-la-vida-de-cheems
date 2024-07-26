d5af8fe8d2bb26cf5eb0e2de328e13a0a9853089
LeoCheems <107894780+execute999@users.noreply.github.com>
2024-07-25 23:23:34 -0400

    Create max index with the minimun element.cpp

---
+++ max index with the minimun element.cpp
@@ -0,0 +1,21 @@
+  int n; cin >> n;
+	vector<ll> A(n); FOR(i, 0, n) cin >> A[i];
+	vector<ll> prev(n, 1), nxt(n, n);
+	stack<int> ps, ns;
+	FOR(i, 0, n) {
+		while(ns.size() && A[ns.top()] > A[i]) {
+			nxt[ns.top()] = i; ns.pop();
+		}
+		ns.push(i);
+	}
+	for(int i = n - 1; i >= 0; i --) {
+		while(ps.size() && A[ps.top()] > A[i])  {
+			prev[ps.top()] = i + 2;
+			ps.pop();
+		}
+		ps.push(i);
+	}
+6
+2 4 4 9 4 9
+? prev: [ 1 , 2 , 2 , 4 , 2 , 6 ]
+? nxt: [ 6 , 6 , 6 , 4 , 6 , 6 ]
