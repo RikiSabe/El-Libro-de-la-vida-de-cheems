a3372124378e43402a7dac7a2b77a1a9abe95c6c
LeoCheems <107894780+execute999@users.noreply.github.com>
2024-08-11 20:00:00 -0400

    Update python cosas.py

--- Miscelanea/python cosas.py
+++ Miscelanea/python cosas.py
@@ -29,3 +29,27 @@ print(ss)
 // fast input
 import sys
 input = sys.stdin.readline
+
+
+// precision en digitos: 
+from decimal import Decimal, getcontext, ROUND_UP
+
+a, b, k = input().split()
+k = int(k)
+getcontext().prec = k + 10
+a = Decimal(a)
+b = Decimal(b)
+resultado = a / b
+quantize_format = Decimal('1.' + '0' * k)
+redondeado = resultado.quantize(quantize_format, rounding=ROUND_UP)
+print(redondeado)
+
+
+from decimal import *
+getcontext().prec = 100
+// getcontext().rounding() = ROUND_UP
+s = Decimal(2).sqrt()
+print(s)
+
+import bisect
+index = biset.bisect_left(s,num) # lista, numero, LOWERBOUND
