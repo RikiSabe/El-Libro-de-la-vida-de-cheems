cd9ea4305abc4ecfe86c002f4685e94a89b638d8
LeoCheems <107894780+execute999@users.noreply.github.com>
2024-09-18 10:15:04 -0400

    Update sieve.cpp

--- math/sieve.cpp
+++ math/sieve.cpp
@@ -1,6 +1,21 @@
 bitset<N> marked;
 vector<int> primis;
 
+vector<ii> Factor(int x) {
+	vector<ii> factor;
+	for(int i = 0; primis[i] * primis[i] <= x; i ++) {
+		if(x % primis[i] == 0) {
+			int c = 0;
+			while(x % primis[i] == 0) {
+				x /= primis[i]; c ++;
+			}
+			factor.pb(ii(primis[i], c));
+		}
+	}
+	if(x != 1) factor.pb(ii(x, 1));
+	return factor;
+}
+
 void init() {
 	
   marked[0] = marked[1] = 1;
