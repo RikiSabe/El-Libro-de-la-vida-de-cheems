4d1397a73484fa9af514f108041d51834c3156cd
LeoCheems <107894780+execute999@users.noreply.github.com>
2024-09-28 15:38:31 -0400

    Create FenwickTree2D.cpp

---
+++ Estructuras/FenwickTree2D.cpp
@@ -0,0 +1,38 @@
+class FenwickTree2D {
+private:
+    int n, m;
+    vector<vector<int>> tree; 
+
+public:
+    FenwickTree2D(int n, int m) : n(n), m(m) {
+        tree.resize(n + 1, vector<int>(m + 1, 0));
+    }
+
+    void update(int x, int y, int delta) {
+        for (int i = x; i <= n; i += i & -i) {
+            for (int j = y; j <= m; j += j & -j) {
+                tree[i][j] += delta;
+            }
+        }
+    }
+
+    int query(int x, int y) {
+        int sum = 0;
+        for (int i = x; i > 0; i -= i & -i) {
+            for (int j = y; j > 0; j -= j & -j) {
+                sum += tree[i][j];
+            }
+        }
+        return sum;
+    }
+
+    int rangeQuery(int x1, int y1, int x2, int y2) {
+        return query(x2, y2) 
+             - query(x1 - 1, y2) 
+             - query(x2, y1 - 1) 
+             + query(x1 - 1, y1 - 1);
+    }
+};
+
+fenwick.update(x, y, val); // (index by 1)
+fenwick.rangeQuery(x1, y1, x2, y2) x1 <= x2, y1 <= y2
