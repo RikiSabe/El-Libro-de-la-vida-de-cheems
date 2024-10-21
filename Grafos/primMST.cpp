896c0af353009876f30acfba61c2dcecde25365f
RikiSabe <61205836+RikiSabe@users.noreply.github.com>
2024-10-20 23:12:14 -0400

    Update primMST.cpp

--- Grafos/primMST.cpp
+++ Grafos/primMST.cpp
@@ -1,28 +1,30 @@
-vector<vector<int>> adj[N];
-// Return the total sum of the Minimum Spanning Tree
-int prim(int start) {
+vector<ii> adj[N];
+
+int primMST(int start, int n) { // start = 1 hasta n
     int cost = 0;
     vector<int> dis(N, inf), parent(N);
     vector<bool> vis(N);
     set<vector<int>> s;
     dis[start] = 0;
     s.insert({0, start});
+    //int band = -1; // control para ver si se puede contruir todos los nodos
     while(!s.empty()){
         auto x = *(s.begin());
         s.erase(x);
         vis[x[1]] = true;
-        int u = x[1], v = parent[x[1]], w = x[0];
+        int u = x[1], w = x[0];
         cost += w;
+        //band++;
         for(auto it : adj[x[1]]){
-            if( vis[it[0]] ) continue;
-            if( dis[it[0]] > it[1] ){
-                s.erase({dis[it[0]], it[0]});
-                dis[it[0]] = it[1];
-                s.insert({dis[it[0]], it[0]});
-                parent[it[0]] = x[1];
+            if( vis[it.first] ) continue;
+            if( dis[it.first] > it.second ){
+                s.erase({dis[it.first], it.first});
+                dis[it.first] = it.second; 
+                s.insert({dis[it.first], it.first});
+                parent[it.first] = x[1];
             }
         }
     }
+    //return (band == n - 1)? cost : -1;
     return cost;
 }
-// Should start node inicial = (1 or 0)
