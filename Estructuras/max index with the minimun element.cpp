2c8bd8e538aa8f0dbe94644ae88c82939ff620f4
LeoCheems <107894780+execute999@users.noreply.github.com>
2025-09-24 10:44:10 -0400

    Update max index with the minimun element.cpp

--- Estructuras/max index with the minimun element.cpp
+++ Estructuras/max index with the minimun element.cpp
@@ -1,21 +1,36 @@
-  int n; cin >> n;
-	vector<ll> A(n); FOR(i, 0, n) cin >> A[i];
-	vector<ll> prev(n, 1), nxt(n, n);
-	stack<int> ps, ns;
-	FOR(i, 0, n) {
-		while(ns.size() && A[ns.top()] > A[i]) {
-			nxt[ns.top()] = i; ns.pop();
-		}
-		ns.push(i);
-	}
-	for(int i = n - 1; i >= 0; i --) {
-		while(ps.size() && A[ps.top()] > A[i])  {
-			prev[ps.top()] = i + 2;
-			ps.pop();
-		}
-		ps.push(i);
-	}
-6
-2 4 4 9 4 9
-? prev: [ 1 , 2 , 2 , 4 , 2 , 6 ]
-? nxt: [ 6 , 6 , 6 , 4 , 6 , 6 ]
+	vector<int> leftMin(n), rightMin(n);
+    vector<int> leftMax(n), rightMax(n);
+
+    // Monotonic stack for minimum (increasing)
+    stack<int> sMin;
+    for (int i = 0; i < n; ++i) {
+        while (!sMin.empty() && a[sMin.top()] > a[i])
+            sMin.pop();
+        leftMin[i] = sMin.empty() ? i + 1 : i - sMin.top();
+        sMin.push(i);
+    }
+
+    while (!sMin.empty()) sMin.pop();
+    for (int i = n - 1; i >= 0; --i) {
+        while (!sMin.empty() && a[sMin.top()] >= a[i])
+            sMin.pop();
+        rightMin[i] = sMin.empty() ? n - i : sMin.top() - i;
+        sMin.push(i);
+    }
+
+    // Monotonic stack for maximum (decreasing)
+    stack<int> sMax;
+    for (int i = 0; i < n; ++i) {
+        while (!sMax.empty() && a[sMax.top()] < a[i])
+            sMax.pop();
+        leftMax[i] = sMax.empty() ? i + 1 : i - sMax.top();
+        sMax.push(i);
+    }
+
+    while (!sMax.empty()) sMax.pop();
+    for (int i = n - 1; i >= 0; --i) {
+        while (!sMax.empty() && a[sMax.top()] <= a[i])
+            sMax.pop();
+        rightMax[i] = sMax.empty() ? n - i : sMax.top() - i;
+        sMax.push(i);
+    }
