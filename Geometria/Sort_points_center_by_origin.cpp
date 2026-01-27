c77e33d9cf90be3c1ea7b5199802e3f0faac8341
LeoCheems <107894780+execute999@users.noreply.github.com>
2026-01-26 21:10:53 -0400

    Update Sort_points_center_by_origin.cpp

--- Geometria/Sort_points_center_by_origin.cpp
+++ Geometria/Sort_points_center_by_origin.cpp
@@ -6,7 +6,7 @@ ll cross(const point &a, const point &b) {
     return a.x * b.y - a.y * b.x;
 }
 
-bool cmp(const point &a, const point &b) {
+bool cmp(const point &a, const point &b) { // sort counter clockwise
     ll ah = (a.y < 0 or (a.y == 0 and a.x < 0));
     ll bh = (b.y < 0 or (b.y == 0 and b.x < 0));
     if (ah != bh) return ah < bh;
